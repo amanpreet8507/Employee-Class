fbb9fa04cd431e81fffef6052c4a802324e8b6e0
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-10-11 00:36:04 -0700

    Initial Commit

---
+++ Employee Class/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Employee Class
+//
+//  Created by Amanpreeet Sandhu on 2024-10-11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
